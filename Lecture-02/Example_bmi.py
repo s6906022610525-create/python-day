7bace61b6d74bc01bdd987d58e51c034bc867b4c
s6906022610525-create <s6906022610525@email.kmutnb.ac.th>
2026-07-07 18:56:17 +0700

    sawatdee1

---
+++ Lecture-02/Example_bmi.py
@@ -0,0 +1,4 @@
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
+bmi = weight / (height ** 2)
+print("Your BMI is:", format(bmi, '.2f'))
