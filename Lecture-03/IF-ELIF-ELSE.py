a8769a27747817ddbcec3260f01816647195f174
s6906022610525-create <s6906022610525@email.kmutnb.ac.th>
2026-07-16 11:21:26 +0700

    sawatdee2.1

--- Lecture-03/IF-ELIF-ELSE.py
+++ Lecture-03/IF-ELIF-ELSE.py
@@ -1,7 +1,9 @@
-num = float(input("Enter a number: "))
-if num > 0:
-    print("positive number.")
-elif num == 0:
-    print("Zero.")
+inchar = input("Input one character:")
+if inchar >= 'A' and inchar <= 'Z':
+    print("You input an Upper Case Letter ", inchar)
+elif inchar >= 'a' and inchar <= 'z':
+    print("You input a Lower Case Letter ", inchar)
+elif inchar >= '0' and inchar <= '9':
+    print("You input Number ", inchar)
 else:
-    print("Negative number.")
+    print("It's not a letter or number.", inchar)
