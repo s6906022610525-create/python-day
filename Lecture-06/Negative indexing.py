a526344f5ef8b2f3eeff02c4bb0bd3cd3691f3ce
s6906022610525-create <s6906022610525@email.kmutnb.ac.th>
2026-08-13 11:12:04 +0700

    sawatdeehub

---
+++ Lecture-06/Negative indexing.py
@@ -0,0 +1,6 @@
+colors = ["red", "green", "blue", "yellow", "purple"]
+second_to_last_color = colors[-2]
+print(f"Second to last color is: {second_to_last_color}")
+# ใช้len
+color = colors[len(colors) - 1]
+print(f"Second to last color is: {color}")
---
+++ Lecture-06/Two-dimensional lists.py
@@ -0,0 +1,7 @@
+matrix=[0][1] = 10
+print(matrix)
+for row in matrix:
+    for element in row:
+        print(element, end=" ")
+    print()
+    
